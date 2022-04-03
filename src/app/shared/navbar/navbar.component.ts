84a8a8ee29da7fdaa3992e4077e6cc2629b62694
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2022-04-03 15:32:00 +0200

    change material icons for FA icons

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FA_ICONS} from '../fa-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  faHome = FA_ICONS.solid.faHome;
+  faChartLine = FA_ICONS.solid.faChartLine;
+  faCog = FA_ICONS.solid.faCog;
+  faBook = FA_ICONS.solid.faBook;
+  faUsers = FA_ICONS.solid.faUsers;
+  faGlobe = FA_ICONS.solid.faGlobe;
+  faUser = FA_ICONS.solid.faUser;
+  faBookReader = FA_ICONS.solid.faBookReader;
+  faBars = FA_ICONS.solid.faBars;
 
   constructor() { }
 
