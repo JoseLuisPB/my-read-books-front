4e365babfac1a68a073eb59e0dbbe766b0b4cce9
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2022-04-16 10:41:35 +0200

    Add book service, book model and remove book interface

---
+++ src/app/services/books.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { map } from 'rxjs/operators'
+import { Book } from "../models/book.model";
+
+@Injectable({providedIn: 'root'})
+export class BooksService {
+
+  url: string;
+  constructor(private http: HttpClient){
+    this.url = '/book/'
+  }
+
+  loadLastNBooks(records: number): Observable<Book[]> {
+    return this.http.get(`${this.url}lastNBooks/${records}`).pipe( map( resp => {
+      console.log(resp);
+      return resp as Book[];
+    }));
+  }
+}
