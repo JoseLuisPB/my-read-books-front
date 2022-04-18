bc82e88f791175cd3fad404362118791291e5d68
Jose Luis Peñuela <jpenuelaberenguer@gmail.com>
2022-04-18 12:31:02 +0200

    Adjust bookservice call with error and prepare to unsubscribe

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,27 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BooksService } from 'src/app/services/books.service';
 import { Book } from 'src/app/models/book.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   headerData: string[] = ['author', 'title', 'year'];
   tableData: Book[] = [];
   isLoading = true;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private booksService: BooksService
   ) {}
 
   ngOnInit(): void {
-    this.booksService.loadLastNBooks(10).subscribe( resp => {
-      this.tableData = resp;
-      this.isLoading = false;
-    });
+    this.subscriptions.push(
+      this.booksService.loadLastNBooks(10).subscribe( resp => {
+        this.tableData = resp;
+        this.isLoading = false;
+      }, error => {
+        console.error(error);
+        this.isLoading = false;
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscriptions.forEach( subscription => subscription.unsubscribe());
+  }
 }
